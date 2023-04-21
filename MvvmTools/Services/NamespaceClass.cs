401944765df3258efbd5c41ef3d96237e078eb90
Chris Bordeman <cbordeman@21stmortgage.com>
2023-04-21 07:11:49 -0400

    Fixed a bug with searching for vms.

---
+++ MvvmTools/Services/NamespaceClass.cs
@@ -0,0 +1,14 @@
+﻿namespace MvvmTools.Services
+{
+    public class NamespaceClass
+    {
+        public NamespaceClass(string @namespace, string @class)
+        {
+            this.Namespace = @namespace;
+            this.Class = @class;
+        }
+
+        public string Namespace { get; set; }
+        public string Class { get; set; }
+    }
+}
---
+++ MvvmTools/Services/ProjectItemAndType.cs
@@ -0,0 +1,28 @@
+﻿using EnvDTE;
+
+namespace MvvmTools.Services
+{
+    public class ProjectItemAndType
+    {
+        public ProjectItemAndType(ProjectItem projectItem, NamespaceClass type)
+        {
+            this.ProjectItem = projectItem;
+            this.Type = type;
+        }
+
+        public ProjectItem ProjectItem { get; set; }
+        public NamespaceClass Type { get; set; }
+
+        public string RelativeNamespace
+        {
+            get
+            {
+                if (this.Type.Namespace == this.ProjectItem?.ContainingProject?.Name)
+                    return "(same)";
+                if (this.Type.Namespace.StartsWith(this.ProjectItem.ContainingProject.Name))
+                    return this.Type.Namespace.Substring(this.ProjectItem.ContainingProject.Name.Length);
+                return this.Type.Namespace;
+            }
+        }
+    }
+}
