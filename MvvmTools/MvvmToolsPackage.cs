b7a88c3def6ef428726aceaede41f55a6799a483
Chris Bordeman <cbordeman@outlook.com>
2023-04-21 07:55:18 -0400

    Advanced to 0.4.0.1 and published

--- MvvmTools/MvvmToolsPackage.cs
+++ MvvmTools/MvvmToolsPackage.cs
@@ -53,7 +53,7 @@ namespace MvvmTools
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(Constants.GuidPackage)]
     ////[ProvideAutoLoad(UIContextGuids.SolutionExists)]
-    [InstalledProductRegistration("MVVM Tools", "Provides access to your corresponding View/ViewModel via Ctrl+E,Q.", "0.4.0.0")]
+    [InstalledProductRegistration("MVVM Tools", "Provides access to your corresponding View/ViewModel via Ctrl+E,Q.", "0.4.0.1")]
     [ProvideAutoLoad(VSConstants.UICONTEXT.ShellInitialized_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class MvvmToolsPackage : AsyncPackage, IMvvmToolsPackage
         //, IAsyncLoadablePackageInitialize
